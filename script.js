a6f7b387ed020156a52d39633aa0c93a9102509f
Harsh Palkar <explorewitharsh@outlook.com>
2022-05-08 17:41:58 +0530

    master

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const timeEl = document.getElementById("time");
+const timeEl = document.querySelector("#time");
 const dateEl = document.getElementById("date");
 const currentWeatherItems = document.getElementById("current-info");
 const timeZone = document.getElementById("timeZone");
@@ -6,16 +6,46 @@ const country = document.getElementById("country");
 const weatherForecastEl = document.getElementById("weather-forecast");
 const currentTemp = document.getElementById("current-temp");
 
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 //setting the time interval so that the time changes at every interval we give by using the setinterval call back funtion
-
 setInterval(() => {
   const time = new Date();
   const month = time.getMonth();
   const date = time.getDate();
   const day = time.getDay();
-  const hours = time.getHours();
-  const minute = time.getMinutes();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  const ampm = hour >= 12 ? "PM" : "AM";
 
-  timeEl.innerHTML = hours + ":" + minute;
-  //   dateEl.innerHTML = date + day;
-}, 1000);
+  timeEl.innerHTML =
+    (hoursIn12HrFormat < 10 ? "0" + hoursIn12HrFormat : hoursIn12HrFormat) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    " " +
+    `<span id="am-pm">${ampm}</span>`;
+
+  dateEl.innerHTML = days[day] + "," + " " + +date + " " + months[month];
+}, 100);
